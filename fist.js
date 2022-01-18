be0bac26341d66cfcf0c1387e8f09e8f17b4f303
ramirezmz <roberto.paolo@dremio.com>
2022-01-17 23:15:41 -0300

    new: refact code + add error function

--- fist.js
+++ fist.js
@@ -1,9 +1,16 @@
 import chalk from "chalk";
 import fs from "fs";
 
+function trataErro(erro) {
+  throw new Error(chalk.red(erro.code, "Nao ha arquivos no caminho"));
+}
+
 function getElement(pathDoc) {
   const encoding = "utf-8";
-  fs.readFile(pathDoc, encoding, (_, data) => {
+  fs.readFile(pathDoc, encoding, (erro, data) => {
+    if (erro) {
+      trataErro(erro);
+    }
     console.log(chalk.green(data));
   });
 }
